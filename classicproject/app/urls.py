ec566217f7d5c412a3c2f9ff953b7c913e458f2a
anil <anilpanwar2031@gmail.com>
2022-11-24 13:38:02 +0530

    product templates

--- classicproject/app/urls.py
+++ classicproject/app/urls.py
@@ -4,4 +4,6 @@ from app import views
 
 urlpatterns = [
     path('', views.index),
+    path('quotation/', views.quotation, name='quotation'),
+    path('products/', views.products, name='products'),
     ]
--- classicproject/app/views.py
+++ classicproject/app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, redirect, HttpResponse
+from .models import Quotation, Product
 
 
 def index(request):
-    return render(request, "index.html",)
+    return render(request, "home.html",)
+
+
+def quotation(request):
+    quots = Quotation.objects.all()
+    return render(request, "quotation.html", {"quots":quots})
+
+
+def products(request):
+    prods = Product.objects.all()
+    return render(request, "product.html", {"prods":prods})
 
 
