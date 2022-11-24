357c18748a5d5886c576a14785302898d4ba3bc3
anil <anilpanwar2031@gmail.com>
2022-11-24 11:27:34 +0530

    Migrations till client

--- classicproject/app/admin.py
+++ classicproject/app/admin.py
@@ -1,20 +1,32 @@
 from django.contrib import admin
-from .models import (
- Quotation,
- Product,
- Client,
-)
+from .models import (Quotation, Section, Product,
+                     Subsection,
+                     Client
+                     )
+
 
 @admin.register(Quotation)
-class CustomerModelAdmin(admin.ModelAdmin):
- list_display = ['id', 'quot_no', 'name']
+class QuotationModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'quot_no', 'name']
+
+
+@admin.register(Section)
+class SectionModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'quotation_id']
+
 
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
- list_display = ['id', 'name', 'quantity',  'description']
+    list_display = ['id', 'name', 'description', 'quantity']
+
+
+@admin.register(Subsection)
+class SubsectionectionModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'section_id']
+
 
 @admin.register(Client)
-class CartModelAdmin(admin.ModelAdmin):
- list_display = ['id', 'name', 'surname']
+class ClientModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'surname']
 
 
--- classicproject/app/models.py
+++ classicproject/app/models.py
@@ -6,51 +6,71 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Quotation(models.Model):
     quot_no = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-    quot_status = models.CharField(max_length=200)
+    # quot_status = models.CharField(max_length=200)
     market_seg = models.CharField(max_length=200)
-    creation_on = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    quot_total = models.IntegerField()
+    # creation_on = models.DateTimeField(auto_now_add=True)
+    # created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    # quot_total = models.IntegerField()
 
     def __str__(self):
         # return self.user.username
         return str(self.id)
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=100)
+    quotation_id = models.ForeignKey(Quotation, on_delete=models.CASCADE)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     quantity = models.PositiveIntegerField(default=0)
     selling_price = models.FloatField()
-    prodtype = models.CharField(max_length=100)
-    unitsale = models.CharField(max_length=100)
-    unitsale2 = models.CharField(max_length=100)
-    factor_1 = models.CharField(max_length=100)
-    is_flooring_product = models.BooleanField(default=False)
-    m2_price = models.BooleanField(default=False)
-    active_field = models.BooleanField(default=False)
-    product_image = models.ImageField(upload_to='productimg')
 
+#
+#     # prodtype = models.CharField(max_length=100)
+#     # unitsale = models.CharField(max_length=100)
+#     # unitsale2 = models.CharField(max_length=100)
+#     # factor_1 = models.CharField(max_length=100)
+#     # is_flooring_product = models.BooleanField(default=False)
+#     # m2_price = models.BooleanField(default=False)
+#     # active_field = models.BooleanField(default=False)
+#     # product_image = models.ImageField(upload_to='productimg')
+#
     def __str__(self):
         return str(self.id)
+
+
+class Subsection(models.Model):
+    name = models.CharField(max_length=100)
+    section_id = models.ForeignKey(Section, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Quotation_item(models.Model):
+    quotation_id = models.ForeignKey(Quotation, on_delete=models.CASCADE)
+    subsection_id = models.ForeignKey(Subsection, on_delete=models.CASCADE)
+
 
 class Client(models.Model):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
-    telephone = models.CharField(max_length=200)
-    mobile = models.CharField(max_length=200)
+    # telephone = models.CharField(max_length=200)
+    # mobile = models.CharField(max_length=200)
     email = models.EmailField(max_length=254)
-    address = models.CharField(max_length=200)
-    notes = models.CharField(max_length=200)
-    method = models.CharField(max_length=200)
-    city = models.CharField(max_length=50)
-    country = models.CharField(max_length=100)
-    suburb = models.CharField(max_length=100)
-    province = models.CharField(max_length=100)
-    postalcode = models.IntegerField()
-    address1 = models.CharField(max_length=100)
-    address2 = models.CharField(max_length=100)
-    notes1= models.CharField(max_length=100)
-
+    quotation_id = models.ForeignKey(Quotation, on_delete=models.CASCADE)
+    # address = models.CharField(max_length=200)
+    # notes = models.CharField(max_length=200)
+    # method = models.CharField(max_length=200)
+    # city = models.CharField(max_length=50)
+    # country = models.CharField(max_length=100)
+    # suburb = models.CharField(max_length=100)
+    # province = models.CharField(max_length=100)
+    # postalcode = models.IntegerField()
+    # address1 = models.CharField(max_length=100)
+    # address2 = models.CharField(max_length=100)
+    # notes1= models.CharField(max_length=100)
 
     def __str__(self):
         # return self.user.username
