47c8f1286b8a0cd30f2e04954ce1e816b30e0004
anil <anilpanwar2031@gmail.com>
2022-11-29 16:44:23 +0530

    quotation page 1

--- classicproject/app/urls.py
+++ classicproject/app/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from app import views
 
 
-urlpatterns = [
-    # path('', views.index),
-    path('', views.dashboard, name='dashboard'),
-    path('quotations/', views.quotation, name='quotations'),
-    path('products/', views.products, name='products'),
-    path('quot/<int:pk>', views.quotdetail, name='quotdetail'),
+urlpatterns = \
+    [
+        # path('', views.index),
+        path('', views.dashboard, name='dashboard'),
+        path('quotations/', views.quotation, name='quotations'),
+        path('products/', views.products, name='products'),
+        path('quot/<int:pk>', views.quotdetail, name='quotdetail'),
     ]
